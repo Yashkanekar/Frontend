cfeba27076ca651befe1e32037c7712a9fd0ed91
yashk <yskanekar@tataadvancedsystems.com>
2025-08-05 14:35:03 +0530

    Refactor myFilter implementation

--- pollyfills/filter.js
+++ pollyfills/filter.js
@@ -5,17 +5,29 @@ arr.filter((item, index, array)=>{
 
 */
 
-Array.prototype.myFilter = function (cb){
-    let temp  = []
-    for(let i = 0; i < this.length; i++){
-        if(cb(this[i], i, arr)) temp.push(this[i])
-    }
-    return temp
+Array.prototype.myFilter = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, arr)) temp.push(this[i]);
+  }
+  return temp;
+};
+
+const arr = [1, 2, 3, 4];
+const newArr = arr.myFilter((item, i, array) => {
+  return item > 2;
+});
+
+console.log(newArr);
+console.log("this", this);
+
+function fn1(value) {
+  let a = [];
+  for (let i = 0; i < 100000; i++) {
+    a[i] = i * i;
+  }
+
+  return function () {
+    console.log(value);
+  };
 }
-
-const arr = [1,2,3,4]
-const newArr = arr.myFilter((item, i, array)=>{
-    return item > 2
-})
-
-console.log(newArr)
